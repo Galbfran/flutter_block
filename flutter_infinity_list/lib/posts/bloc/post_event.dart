982ac0debf681967392c74d6f6ea79d2b22be10a
franco galbiati <francogalbiati984@gmail.com>
2024-09-17 19:57:57 -0300

    add example complete

--- flutter_infinity_list/lib/posts/bloc/post_event.dart
+++ flutter_infinity_list/lib/posts/bloc/post_event.dart
@@ -1,4 +1,8 @@
 part of 'post_bloc.dart';
 
-@immutable
-sealed class PostEvent {}
+sealed class PostEvent extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+final class PostFetched extends PostEvent {}
